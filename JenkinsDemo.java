9e1a6ef934e76f43d551ff5cd1dd1f8b1866cbaf
Keerthana D <dkeerthikavin@gmail.com>
2021-03-10 16:41:36 +0530

    Added content

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -2,6 +2,5 @@ class JenkinsDemo{
     public static void main(String[] args){
         System.out.println("Welcome to Jenkins Demo");
         System.out.println("Added content");
-        System.out.println("Hello....Have a nice day");
     }
 }
