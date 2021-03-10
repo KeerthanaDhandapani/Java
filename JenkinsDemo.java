352be26e5018c16f9624c7d7bcbf8b8a36519cd4
Keerthana D <dkeerthikavin@gmail.com>
2021-03-10 15:26:49 +0530

    Rectified errors

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,5 +1,5 @@
 public class JenkinsDemo{
-    public static vaoid main(String[] args){
+    public static void main(String[] args){
         System.out.println("Welcome to Jenkins Demo");
         System.out.println("Added content");
     }
