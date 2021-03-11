5a74477ae1eb4a5f4c388fad7f71659d47561ad5
KeerthanaDhandapani <74096537+KeerthanaDhandapani@users.noreply.github.com>
2021-03-11 22:43:20 +0530

    Update JenkinsDemo.java

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,7 +1,6 @@
 class JenkinsDemo{
     public static void main(String[] args){
         System.out.println("Welcome to Jenkins Demo");
-        System.out.println("Added content");
         System.out.println("Hello....Have a nice day");
         System.out.println("Hello :)");
     }
